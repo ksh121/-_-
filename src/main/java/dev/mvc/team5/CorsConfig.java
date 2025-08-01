78852dc69f5df64cb3b124ca7fb08600229fc7d3
kimgyuhwa <ggyu0825@gmail.com>
2025-08-01 15:49:39 +0900

    서버

--- src/main/java/dev/mvc/team5/CorsConfig.java
+++ src/main/java/dev/mvc/team5/CorsConfig.java
@@ -8,11 +8,12 @@ public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-      .allowedOrigins("http://121.78.128.212:3000", "http://localhost:3000")  // 실제 클라이언트 주소
+    registry.addMapping("/**") // 모든 경로에 대해
+      .allowedOriginPatterns("*")    // 모든 origin 허용
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-      .allowedHeaders("*")
-      .allowCredentials(true)  // 인증 허용
-      .maxAge(3600);
+      .allowedHeaders("*")   // 모든 요청 헤더 허용
+      .allowCredentials(true)   // 인증 쿠키/세션 허용
+      .maxAge(3600);   //캐시 시간 1시간
+    
   }
 }
