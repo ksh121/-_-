2e6f3420e210951739926dde7d95cda1b31f8782
kimgyuhwa <ggyu0825@gmail.com>
2025-08-01 16:17:54 +0900

    tq

--- src/main/java/dev/mvc/team5/CorsConfig.java
+++ src/main/java/dev/mvc/team5/CorsConfig.java
@@ -5,15 +5,13 @@ import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**") // 모든 경로에 대해
-      .allowedOriginPatterns("*")    // 모든 origin 허용
+    registry.addMapping("/**")
+      .allowedOrigins("http://121.78.128.212:3000")  // React 주소 명시
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-      .allowedHeaders("*")   // 모든 요청 헤더 허용
-      .allowCredentials(true)   // 인증 쿠키/세션 허용
-      .maxAge(3600);   //캐시 시간 1시간
-    
+      .allowedHeaders("*")
+      .allowCredentials(true)  // 쿠키/세션 사용 시 꼭 필요
+      .maxAge(3600);
   }
 }
