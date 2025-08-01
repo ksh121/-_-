74f2a99e145e71349cea535c0d9033f72cb7a9f6
kimgyuhwa <ggyu0825@gmail.com>
2025-08-01 17:07:47 +0900

    asq1we

--- src/main/java/dev/mvc/team5/Team5V2sbm3cApplication.java
+++ src/main/java/dev/mvc/team5/Team5V2sbm3cApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+//asdasd
 @SpringBootApplication
 @EnableScheduling
 //@ComponentScan(basePackages = {"dev.mvc"})
