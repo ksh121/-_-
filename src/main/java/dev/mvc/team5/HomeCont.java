c9ee3bf7fc4cbafe911aeb489d5a2696b483cc3d
kimgyuhwa <ggyu0825@gmail.com>
2025-08-01 16:41:47 +0900

    3분

--- src/main/java/dev/mvc/team5/HomeCont.java
+++ src/main/java/dev/mvc/team5/HomeCont.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeCont {
   @Autowired // Spring이 CateProcInter를 구현한 CateProc 클래스의 객체를 생성하여 할당
  
@@ -17,6 +18,7 @@ public class HomeCont {
   }
   
   @GetMapping(value="/") // http://localhost:9091
+  
   public String home() { // 파일명 return
     // return "index"; // /templates/index.html
     return "<H1>Spring Boot server 작동중...</H1>";
