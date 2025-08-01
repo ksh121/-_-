ff71f3547395315e688d69527c20e1143dc7cfde
kimgyuhwa <ggyu0825@gmail.com>
2025-08-01 15:24:35 +0900

    zz

--- src/main/java/dev/mvc/team5/CorsConfig.java
+++ src/main/java/dev/mvc/team5/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
       .allowedOriginPatterns("*")    // 모든 origin 허용
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
       .allowedHeaders("*")   // 모든 요청 헤더 허용
-      .allowCredentials(true)   // 인증 쿠키/세션 허용
+//      .allowCredentials(true)   // 인증 쿠키/세션 허용
       .maxAge(3600);   //캐시 시간 1시간
     
   }
